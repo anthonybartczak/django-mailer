dbd28e8f63d62d67fc55354385f17972e96ebcea
anthonybartczak <antoni.bartczak.w@gmail.com>
2021-04-10 18:00:21 +0200

    Mailgun changes

--- mailer/send_mail.py
+++ mailer/send_mail.py
@@ -1,6 +1,7 @@
 from requests import post
 from .models import EmailModel
 from datetime import datetime, date
+from mailguncreds import MAILGUN_API_KEY, MAILGUN_BASE_URL
 
 current_hour = datetime.now().hour
 current_date = date.today()
@@ -12,11 +13,10 @@ if (len(mails) > 0):
         send_message(mail.address, mail.title, mail.content, mail.image)
 
 
-
 def send_message(addresses, subject, content, image):
 	return post(
-		"https://api.mailgun.net/v3/sandboxa773e44539964739872c89c2c4a7cb73.mailgun.org/messages",
-		auth=("api", "d4663b1aa6d7ec66e2383a43c5a052da-73e57fef-8ec9c2a6"),
+		MAILGUN_BASE_URL + "/messages",
+		auth=("api", MAILGUN_API_KEY),
 		data={
             "from": "mailgun@sandboxa773e44539964739872c89c2c4a7cb73.mailgun.org",
 			"to": addresses, #string array
